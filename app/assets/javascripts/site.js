39389728ef53cc66f075e195998916eb292d3e4a
Tery Lim <kumabotz@gmail.com>
2014-10-08 00:13:26 +1300

    Add assets and support ie 8,9 css

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,6 @@
+//= require jquery/jquery.min
+//= require jquery/jquery.scrolly.min
+//= require jquery/jquery.scrollzer.min
+//= require skel/skel.min
+//= require skel/skel-layers.min
+//= require init
